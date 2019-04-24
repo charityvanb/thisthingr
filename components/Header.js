3f2bb350d3c1efeeb8384c27ca5d008f620d40a4
charityvanb <charityvanb@gmail.com>
2019-04-24 16:23:54 -0600

    Here we go

---
+++ components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactNative from 'react-native';
+
+const Texty = () => {
+    return (
+        <Text>Once I was a chicken</Text>
+    );
+};
+
+export default Texty;
