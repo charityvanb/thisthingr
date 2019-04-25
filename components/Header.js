8de045d216a35492d6cd4e415703e7b594f38187
charityvanb <charityvanb@gmail.com>
2019-04-24 22:30:32 -0600

    started styling

--- components/Header.js
+++ components/Header.js
@@ -2,9 +2,17 @@ import React from 'react';
 import { Text } from 'react-native';
 
 const Header = () => {
+    const { textStyle } = styles;
+
     return (
-        <Text>Once I was a chicken</Text>
+        <Text style={textStyle}>Once I was a chicken</Text>
     );
 };
+
+const styles = {
+    textStyle: {
+        fontSize: 20
+    }
+}
 
 export default Header;
