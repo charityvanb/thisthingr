1ff26b9fb08d610bc26eb23725330ee983ac62fd
charityvanb <charityvanb@gmail.com>
2019-04-25 22:31:28 -0600

    added mroe styling

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 const Header = () => {
-    const { textStyle } = styles;
+    const { textStyle, viewStyle } = styles;
 
     return (
+        <View style-={viewStyle}>
         <Text style={textStyle}>Once I was a chicken</Text>
+        </View>
     );
 };
 
 const styles = {
+    viewStyle: {
+        backgroundColor: '#3DCCCC',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        paddingTop: 15
+    },
     textStyle: {
         fontSize: 20
     }
