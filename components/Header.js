fb3d1c31499f18395f2fd87b03672e58580b8072
charityvanb <charityvanb@gmail.com>
2019-04-29 13:22:53 -0600

    fixed CSS problem

--- components/Header.js
+++ components/Header.js
@@ -5,22 +5,22 @@ const Header = () => {
     const { textStyle, viewStyle } = styles;
 
     return (
-        <View style-={viewStyle}>
-        <Text style={textStyle}>Once I was a chicken</Text>
-        </View>
+    <View style={viewStyle}>
+    <Text style={textStyle}>Albums!</Text>
+    </View>
     );
 };
 
 const styles = {
     viewStyle: {
-        backgroundColor: '#3DCCCC',
-        justifyContent: 'center',
+        backgroundColor: '#F8F8F8',
+        justifyContext: 'center',
         alignItems: 'center',
         height: 60,
-        paddingTop: 15
+        paddingTop: 30
     },
     textStyle: {
-        fontSize: 20
+        fontSize:20
     }
 }
 
