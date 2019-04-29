fb36030eb4fbd3c4aae66a45d2e94187ef1bab89
charityvanb <charityvanb@gmail.com>
2019-04-29 17:09:13 -0600

    edited the AlbumList file

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './components/Header'
+import AlbumList from './components/AlbumList.js'
 
 export default class App extends React.Component {
   render() {
     return (
+      <View>
       <Header headerText={'Albums'} />
+      <AlbumList />
+      </View>
     );
   }
 }
---
+++ components/AlbumList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default class AlbumList extends React.Component {
+    render() {
+    return (
+        <View>
+            <Text>Albumn Listy fun times</Text>
+        </View>
+    )
+    }
+}
+
+export default AlbumList;
