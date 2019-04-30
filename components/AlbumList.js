7d14d5b76419efbcdada8ef7ae2b788ad8aa74ed
charityvanb <charityvanb@gmail.com>
2019-04-29 23:17:37 -0600

    added componentWillMount and made sure it worked.

--- components/AlbumList.js
+++ components/AlbumList.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+
 export default class AlbumList extends React.Component {
+    componentWillMount() {
+        console.log('component will mount in AlbumList')
+    }
     render() {
     return (
         <View>
             <Text>Albumn Listy fun times</Text>
         </View>
-    )
+        )  
     }
 }
-
-export default AlbumList;
