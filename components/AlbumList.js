5f0af88fc9a7ffa910adfc3af8b5cd30e7584ea7
charityvanb <charityvanb@gmail.com>
2019-04-30 17:06:00 -0600

    replaced Axios with a regular fetch request due to compatibility issues

--- components/AlbumList.js
+++ components/AlbumList.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 
 export default class AlbumList extends React.Component {
     componentWillMount() {
         console.log('component will mount in AlbumList')
+            axios.get ('https://rallycoding.herokuapp.com/api/music_albums')
+            .then((response) => response.json())
+            .then((responseData) => this.setState({ albums: responseData }))
     }
     render() {
     return (
