8da78531bc5a895fa5c5fe8526eb05b7c0af2c6a
charityvanb <charityvanb@gmail.com>
2019-05-01 13:59:47 -0600

    set the API data to state

--- components/AlbumList.js
+++ components/AlbumList.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import console = require('console');
 
 export default class AlbumList extends React.Component {
+    state = { albums: [] }
+
     componentWillMount() {
             fetch('https://rallycoding.herokuapp.com/api/music_albums')
-            .then(response => console.log(response))
+            .then(response => this.setState({ album: response.data }))
             // .then((response) => response.json())
-            // .then(data => this.setState({ albums: data }));
+            // .then(data => this.setState({ albums: data }))
     }
 
     render() {
+        console.log(this.state)
+        
     return (
         <View>
             <Text>Album Listy fun times</Text>
