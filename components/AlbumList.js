77b01288898028d6083c6c8ece4bd515171e70eb
charityvanb <charityvanb@gmail.com>
2019-05-01 13:09:29 -0600

    Corrected a spelling error

--- components/AlbumList.js
+++ components/AlbumList.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import axios from 'axios';
-
 
 export default class AlbumList extends React.Component {
     componentWillMount() {
         console.log('component will mount in AlbumList')
-            axios.get ('https://rallycoding.herokuapp.com/api/music_albums')
+            fetch('https://rallycoding.herokuapp.com/api/music_albums')
             .then((response) => response.json())
-            .then((responseData) => this.setState({ albums: responseData }))
+            .then(data => this.setState({ albums: data }));
     }
+
     render() {
     return (
         <View>
-            <Text>Albumn Listy fun times</Text>
+            <Text>Album Listy fun times</Text>
         </View>
         )  
     }
